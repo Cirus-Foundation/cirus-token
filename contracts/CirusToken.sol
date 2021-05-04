358bdfc92c500bc4fa834e8fffb90f967f36ebd8
Ollie <3martynov@gmail.com>
2021-05-04 11:01:44 -0400

    change import to lowercase

--- contracts/CirusToken.sol
+++ contracts/CirusToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.4.22 <0.9.0;
 
-import "@openZeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 contract CirusToken is ERC20 {
